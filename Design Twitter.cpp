3f9718e50b500c2a96aef001bad387e4684d2952
jonitox <jonitox>
2025-10-09 22:51:50 -0700

    Design Twitter

---
+++ Design Twitter.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Twitter {
+public:
+    int count = 0;
+    unordered_map<int, unordered_set<int>> follows; // followerId -> set of followeeIds
+    map<int, pair<int,int>, greater<int>> tweets; // count -> {userId, tweetId}
+    
+    Twitter() {
+        count = 0;
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        tweets[count++] = {userId, tweetId};
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        vector<int> ret;
+        for(auto& t: tweets){ // from most recent to least
+            int tweetUserId = t.second.first;
+            if(tweetUserId != userId && 
+            follows[userId].count(tweetUserId) ==0){
+                continue;
+            }
+            ret.push_back(t.second.second);
+            if(ret.size()==10)
+                break;
+        }
+        return ret;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        follows[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        follows[followerId].erase(followeeId);
+    }
+};
+
+int main() {
+}
