ec2b8c220ec0068cbe1a9d60f29aa67a1d7b1fc4
jonitox <jonitox94@naver.com>
2025-10-26 21:54:55 -0700

    minor comments

--- Find the Duplicate Number.cpp
+++ Find the Duplicate Number.cpp
@@ -16,16 +16,25 @@ public:
     /* Fast and Slow Pointer
     int findDuplicate(vector<int>& nums) { 
         int slow = 0, fast = 0;
+        // Phase1: detect cycle(duplicate number)
         while (true) {
             slow = nums[slow];
             fast = nums[nums[fast]];
             if (slow == fast) {
                 break;
             }
-        }
+        } 
+        // When they meet,
+        // Let L = distance from start to cycle start (entrance)
+        // Let C = length of cycle
+        // Let X = distance from cycle start to meeting point
+        // Then, we have: 2(L + X) = L + X + nC  =>  L = nC - X
 
         int slow2 = 0;
-        while (true) {
+        // Phase2: find the entrance(duplicate number)
+        // slow is inside the cycle (distance C - X from entrance of cycle).
+        // Since L = nC - X, after L steps, slow2 (from start) and slow (in cycle) will meet at the entrance.
+        while (true) { 
             slow = nums[slow];
             slow2 = nums[slow2];
             if (slow == slow2) {
