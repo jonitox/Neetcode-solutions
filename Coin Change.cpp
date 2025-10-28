de0fa57d0f8a8410a57185dd09fc8ba732fe3bfb
jonitox <jonitox94@naver.com>
2025-10-27 22:12:54 -0700

    Coin Change

---
+++ Coin Change.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int INF = 99999999;
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int> dp(amount+1, INF);
+        dp[0]= 0;
+        for(int i=0; i<n; ++i)
+            for(int j=0; j<= amount; ++j)
+                dp[j] = min(dp[j], j>=coins[i]? dp[j-coins[i]]+1:INF);
+
+        return dp[amount] == INF? -1: dp[amount];
+    }
+};
+
+
+int main() {
+    // test code (optional)
+    return 0;
+}
