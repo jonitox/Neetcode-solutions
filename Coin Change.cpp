1c794b65f8b5a7c663303b47348325ac0ec6ba57
jonitox <jonitox94@naver.com>
2025-10-27 22:33:10 -0700

    minor comments

--- Coin Change.cpp
+++ Coin Change.cpp
@@ -6,7 +6,7 @@ public:
     int INF = 99999999;
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int> dp(amount+1, INF);
+        vector<int> dp(amount+1, INF); // dp[j]: min number of coins to make amount j
         dp[0]= 0;
         for(int i=0; i<n; ++i)
             for(int j=0; j<= amount; ++j)
