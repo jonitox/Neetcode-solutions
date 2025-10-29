102db83a6cbd413ad97ea61d2065fd78ab1985ba
jonitox <jonitox94@naver.com>
2025-10-28 23:15:56 -0700

    Best Time to Buy and Sell Stock

---
+++ Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans =0;
+        int m =prices.back(); // max price from the right
+        for(int i=prices.size()-2; i>=0; --i){
+            ans = max(ans, m-prices[i]); // sell at m, buy at prices[i]
+            m = max(m, prices[i]);
+        }
+        return ans; 
+    }
+};
+
+int main() {
+    // test code (optional)
+    return 0;
+}
