f1e24f7b1cd8b5ff3f33126a7422c01f048bfda5
jonitox <jonitox94@naver.com>
2025-11-01 01:23:31 -0700

    Koko Eating Bananas

---
+++ Koko Eating Bananas.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int m = 0;
+        for(int& p: piles)
+            m = max(p, m);
+        int lo = 0, hi = m;
+        while(lo+1<hi){ // binary search for the minimum rate that allows Koko to finish in h hours
+            int mid = (lo+hi)/2;
+            int time = 0;
+            for(int&p : piles)
+                time += (p-1)/mid +1;
+            if(time > h)
+                lo = mid; 
+            else
+                hi = mid; 
+        }
+        return hi;
+    }
+};
+
+int main() {
+    // test code (optional)
+    return 0;
+}
