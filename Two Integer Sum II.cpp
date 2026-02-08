779a957372bdb4345a22460bc5d847cab93e0a46
jonitox <jonitox94@naver.com>
2026-02-07 23:09:21 -0800

    minor comments

--- Two Integer Sum II.cpp
+++ Two Integer Sum II.cpp
@@ -6,13 +6,14 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int l= 0, r=numbers.size()-1;
         int sum;
+        // two pointers approach
         while(l<r) {
             sum = numbers[l] + numbers[r];
             if(sum == target)
                 return {l+1, r+1};
-            if(sum < target)
+            if(sum < target)    // need a larger sum
                 l++;
-            else 
+            else    // need a smaller sum
                 r--;
         }
         return {};
