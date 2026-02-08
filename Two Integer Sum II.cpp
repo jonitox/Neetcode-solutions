f37915b677bdc29d3b510b030533e5461d8792b1
jonitox <jonitox94@naver.com>
2026-02-07 23:08:03 -0800

    Two Integer Sum II

---
+++ Two Integer Sum II.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l= 0, r=numbers.size()-1;
+        int sum;
+        while(l<r) {
+            sum = numbers[l] + numbers[r];
+            if(sum == target)
+                return {l+1, r+1};
+            if(sum < target)
+                l++;
+            else 
+                r--;
+        }
+        return {};
+    }
+};
+
+
+
+int main() {
+    // test code (optional)
+    return 0;
+}
