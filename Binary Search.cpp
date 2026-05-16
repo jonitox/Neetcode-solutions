83e16f89abb77fcb8d81e70ee0df80e9318b8626
jonitox <jonitox94@naver.com>
2026-05-15 22:09:30 -0500

    Binary Search

---
+++ Binary Search.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+        while(l+1<r){
+            int mid = (l+r)/2;
+            if(target == nums[mid]) return mid;
+            else if(target<nums[mid]) r = mid;
+            else l = mid; 
+        }
+        return nums[l]==target? l: -1;
+    }
+};
+
+
+int main() {
+    // test code (optional)
+    return 0;
+}
