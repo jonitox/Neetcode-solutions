887b7d39b59871b900db07293f69b2b20c56db47
jonitox <jonitox94@naver.com>
2026-06-30 21:26:12 -0500

    Reverse Linked List

---
+++ Reverse Linked List.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* cur = head;
+        // Reversing a linked list iteratively. Flipping pointers one step at a time.
+        while(cur != nullptr){
+            ListNode* tmp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        return prev;
+    }
+};
+
+
+
+int main() {
+    // test code (optional)
+    return 0;
+}
